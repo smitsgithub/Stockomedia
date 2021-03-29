9a9c7e000414faa3fd18168fc166ed012930dc1f
Nayan <iamnayan160701@gmail.com>
2021-03-29 16:25:28 +0530

    First Commit

---
+++ Index.js
@@ -0,0 +1,13 @@
+//document.addEventListener("DOMContentLoaded", function () {
+//    document.querySelector("#A1").onclick = function () {
+//        if (document.querySelector("#financial_markets").style.display === "none") {
+//            document.querySelector("#financial_markets").style.display === "block"
+//        }
+//    }
+//});
+
+function hello() {
+    document.getElementById("financial_markets").innerhtml = 
+}
+
+
